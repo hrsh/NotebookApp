f65d4a40b0f86ea9ebc4960df17307be81ddb355
hrshojaie <hrshojaie@gmail.com>
2021-03-16 12:55:44 +0330

    add web app (mvc)

---
+++ src/NotebookApp.WebApp/Controllers/NotebookController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotebookApp.WebApp.Controllers
+{
+    [Route("[controller]")]
+    public class NotebookController : Controller
+    {
+        public NotebookController()
+        {
+
+        }
+
+
+    }
+}
